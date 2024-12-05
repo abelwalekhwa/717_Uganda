daccf062fcf26574429183c0bedd6663ca7e35c1
Dominic Delport <Dominic.Delport@burnet.edu.au>
2024-12-05 19:17:01 +1100

    Small tweak to default int/float

--- starsim/gavi/utils.py
+++ starsim/gavi/utils.py
@@ -13,7 +13,7 @@ import sciris as sc
 __all__ = ['parse_age_range', 'set_seed', 'peak_coverage_filter', 'outbreak_detection_trigger', 'ebola_detection_action']
 
 nbbool  = nb.bool_
-nbint   = ss.nbint
+nbint   = nb.float64
 nbfloat = ss.nbfloat
 
 cache = sso.numba_cache # Turning this off can help switching parallelization options
